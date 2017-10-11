2d738f165d298a6bfb143340ab1e1965e995ec41
Joshua Marsden <joshuamarsden@gmail.com>
2017-10-11 11:32:28 -0700

    trying without the .h file

--- c++_programs/main.cpp
+++ c++_programs/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
-#include "query.h"
+//#include "query.h"
 
 using namespace std;
+
+void query(string prompt, string& ans);
 
 int main()
 {
--- c++_programs/query.cpp
+++ c++_programs/query.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "query.h"
+//#include "query.h"
 
 using namespace std;
 
