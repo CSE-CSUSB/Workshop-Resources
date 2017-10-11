d4697f26f5528930d174aabb7baa4c28fef5f2d6
Joshua Marsden <joshuamarsden@gmail.com>
2017-10-11 11:23:02 -0700

    Created a simple C++ program duo for compiling practice

---
+++ c++_programs/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string input = "";
+    
+    while(input != "exit")
+    {
+        query("Please enter the password: ", input);
+        if(input == "linuxrocks")
+        {
+            cout << "Welcome!" << endl;
+            break;
+        }
+        else
+            cout << "Nope. Try again." << endl;
+    }
+
+    return 0;
+}
---
+++ c++_programs/query.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void query(string prompt, string& ans)
+{
+    
+    
+    return 0;
+}
